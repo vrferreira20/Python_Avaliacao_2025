fbfe93543cf86ac83c2cbf060a69220a4594138b
Samuel Felipe do Santos Mendes <101577181+SamuelFelipeMendes@users.noreply.github.com>
2025-09-02 19:37:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import pandas as pd
 
+def LoadData(DATASET):
+  df = None
+  try:
+    df = pd.read_csv(DATASET)
+    print(df)
+  except:
+    print('Load dataset Error!')
+    return df
+def DataTreat(data)
+  print(data.info())
+
+DATASET = 'insurance.csv'
+data = LoadData(DATASET)
