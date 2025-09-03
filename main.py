8dffcddccb352623a0947a10a12243845c74ae76
Samuel <samuelfelipe.live@gmail.com>
2025-09-03 19:17:38 -0300

    Update Main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 def LoadData(DATASET):
   df = None
   
   try:
-    df = pd.read_csv(DATASET,sep=',',encoder='utf8')
+    df = pd.read_csv(DATASET,sep=',')
     print(df)
   except:
     print('Load dataset Error!')
@@ -12,7 +13,29 @@ def LoadData(DATASET):
   return df
     
 def DataTreat(data):
+  #print(data.info())
+
+  data.drop_duplicates(inplace=True)
+
+  data.drop(columns=['availability','society','balcony'], inplace=True)
+
+  data.dropna(inplace=True)
+
+  lb = LabelEncoder()
+
   print(data.info())
 
-DATASET = 'insurance.csv'
+  data['total_sqft'] = lb.fit_transform(data['total_sqft'])
+
+  data['Room'] = data['size'].str.replace(r'[^0-9]','',regex=True)
+
+  print(data['location'].value_counts())
+
+  #print(data.head())
+
+  #print(data.info())
+DATASET = 'DataHouse.csv'
 data = LoadData(DATASET)
+
+if data is not None:
+  DataTreat(data)
