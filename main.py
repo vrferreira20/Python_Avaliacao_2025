a91596281b3528d7be1da056cc2886e8ec01cdf7
Samuel <samuelfelipe.live@gmail.com>
2025-09-10 16:43:18 -0300

    Update Main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
+
 
 def LoadData(DATASET):
     df = None
@@ -21,7 +23,7 @@ def DataTreat(data):
     # ---- Trata total_sqft ----
     def convert_sgft(x): # 'x' representa o valor de cada coluna
         x = str(x) #Converte o valor para string (facilita o tratamento de ranges e textos).
-        if '-' in x:  
+        if '-' in x:
             a, b = x.split('-')
             return (float(a) + float(b)) / 2 #Caso x seja um intervalo (ex.: "2100-2850"), divide em dois valores e tira a média.
         
@@ -37,6 +39,7 @@ def DataTreat(data):
             except:
                 return None #Caso seja apenas um número (ex.: "2100"), converte para float. Se não conseguir converter, retorna None.
 
+
     data['total_sqft'] = data['total_sqft'].apply(convert_sgft) #Aplica a função convert em toda a coluna total_sqft, convertendo os valores para números. ->'apply(convert)' pega cada valor da coluna e passa como argumento para a função.
     #print(data['total_sqft'])
 
@@ -44,12 +47,14 @@ def DataTreat(data):
     data['price'] = (data['price'] * 100000) * 0.063 # Aprica uma conversão na coluna price. Motivo: O preço originalmente está em Lakh/Lac (uma unidade do sistema de numeração indiano), então converto ele primeiro para rúpias indianas (INR) e depois para Real (R$)
     #print(data['price'])
 
-    # ---- Extrai número de quartos ----
-    data['Room'] = data['size'].str.extract(r'(\d+)').astype(float) # Usa um regex (é uma linguagem de padrões usada para encontrar e manipular textos), onde '\d+' extrai apenas os números na coluna size.
+    # ---- Extrai número de quartos para uma nova coluna ----
+    data['room'] = data['size'].str.extract(r'(\d+)').astype(float) # Usa um regex (é uma linguagem de padrões usada para encontrar e manipular textos), onde '\d+' extrai apenas os números na coluna size.
 
-    
+    lb = LabelEncoder()
+    data['location'] = lb.fit_transform(data['location'])
+    print(data.head())
+
     return data
-
 def dispersao_grafico(data, x_col, y_col):
     plt.figure(figsize=(8,6))
     plt.scatter(data[x_col], data[y_col], color = 'blue', alpha=0.6) # Plota todos os pontos de uma vez, colorindo pela classe
@@ -66,11 +71,12 @@ def distribuicao_preco(data):
     plt.show()
 
 def boxplot_room_price(data):
-    sns.boxplot(data=data, x="Room", y="price")
+    sns.boxplot(data=data, x="total_sqft", y="price")
     plt.xticks(rotation=45)
     plt.title("Distribuição de preço por número de quartos")
     plt.show()
 
+#esse heatmap trás uma correlação não muito relevante
 def correlacao_val_num(data):
     corr = data[["price", "total_sqft", "bath"]].corr()
     sns.heatmap(corr, annot=True, cmap="coolwarm")
@@ -92,7 +98,7 @@ if data is not None:
     data = DataTreat(data)
     #dispersao_grafico(data, x_col='total_sqft', y_col='price')
     #distribuicao_preco(data)
-    #boxplot_size_price(data)
+    #boxplot_room_price(data)
     #correlacao_val_num(data)
     #preco_med(data)
 
--- teste_projeto.py
+++ teste_projeto.py
@@ -1,27 +1,27 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+
 
 #Comando para ler o CSV
-'''
-data = pd.read_csv('bengaluru_house_prices.csv')
-print (data)
-'''
 #Função de carregar os dados
-def uploadData(nomeArquivo):
-    data_up = None
+def uploadData(NOMEARQUIVO):
+    df = None
     try:
-        data_up = pd.read_csv(nomeArquivo, sep=',')
-
+        df = pd.read_csv(NOMEARQUIVO, sep=',')
     except:
         print("Erro no upload de dados")
     
-    return data_up
+    return df
 
 #Função para preparar os dados
-def prepareData(p_data):
+def prepareData(data):
     print ('Preparando dados')
-    
+
+    lb = LabelEncoder()
     # verificar informações rápidas do arquivo: tipos, nome, dados nulos
-    #print(p_data.info())
+    #print(data.info())
 
     # retorna as primeiras 5 linhas do arquivo
     #print(p_data.head())
@@ -32,36 +32,50 @@ def prepareData(p_data):
     # retorna uma estatistica básica sobre as colunas
     #print(p_data.describe())
 
-    # remove os registros duplicados
-    #p_data.drop_duplicates(inplace=True)
-    #p_data.drop(columns=['bath', 'balcony'], inplace=True)
-    #print(p_data)
+    #remove os registros duplicados
+    data.drop_duplicates(inplace=True)
+    data.drop(columns=['balcony','availability','society'], inplace=True)
 
     # remove os dados nulos do dataset
     #p_data.dropna(inplace=True)
     #print(p_data)
-
     # Selecionando apenas uma coluna
     #print(p_data['size'])
-
     # fatiamento para retornar multiplas linhas
-    #print (p_data[0:2])
-
+    #print (p_data[0:2]
     # fatiamento para retornar uma linha específica com base no índice
     #print (p_data.loc[10])
 
     #Ordenamento do DataFrame por valores de uma ou mais colunas
     #p_data.sort_values(by=['size', 'price'], ascending=False, inplace=True)
     #print(p_data)
-
     #Mudando o nome das colunas
     #p_data.rename(columns={'price': 'preco'}, inplace = True)
-    print(p_data)
 
+    # Usa um regex (é uma linguagem de padrões usada para encontrar e manipular textos), onde '\d+' extrai apenas os números na coluna size.
+    data['room'] = data['size'].str.extract(r'(\d+)').astype(float)
+
+    data['location'] = lb.fit_transform(data['location'])
+
+    data.drop(columns=['size'], inplace=True)
+
+    #print(data['location'].value_counts())
+
+    #print(data.info())
+
+    #print(data.head())
+
+    return data
+
+def heatMap(data):
+    df = (data[['price','room','bath']].corr())
+    sns.heatmap(df, annot=True, cmap="viridis", cbar=True)
+    plt.show()
 
 #Espaço para definição do arquivo
-NOMEARQUIVO = 'bengaluru_house_prices.csv'
-data_nome = uploadData(NOMEARQUIVO)
+NOMEARQUIVO = 'DataHouse.csv'
+data = uploadData(NOMEARQUIVO)
 
-if data_nome is not None:
-    prepareData(data_nome)
+if data is not None:
+    prepareData(data)
+    heatMap(data)
